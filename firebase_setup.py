6fe13ffceb5ec65aefc7841cacb19ff396eecebe
Nathaniel <nathaniel@victorymodeling.com>
2024-08-08 19:21:43 -0400

    Handling exception from credential not being found

--- firebase_setup.py
+++ firebase_setup.py
@@ -1,6 +1,11 @@
 import firebase_admin
 from firebase_admin import credentials, auth
 import os
+import json
 
-cred = credentials.Certificate(os.path.join(os.getcwd(), 'firebase_service_cred.json'))
+try:
+    cred = credentials.Certificate(os.path.join(os.getcwd(), 'firebase_service_cred.json'))
+except json.JSONDecodeError:
+    cred = credentials.Certificate(json.loads(os.environ['FIREBASE_SERVICE_CRED']))
+
 firebase_admin.initialize_app(cred)
